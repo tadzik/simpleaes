de66fd5341fa2f3b6137f6a4079339d3e590a989
Tadeusz Sośnierz <tadzikes@gmail.com>
2013-04-30 19:59:56 +0200

    Add docs

--- simpleaes/simpleaes.go
+++ simpleaes/simpleaes.go
@@ -1,3 +1,4 @@
+// Make encryption in Go easy
 package simpleaes
 
 import (
@@ -9,6 +10,13 @@ type Aes struct {
     enc, dec cipher.BlockMode
 }
 
+// Creates a new encryption/decryption object
+// with a given key of a given size
+// (16, 24 or 32 for AES-128, AES-192 and AES-256 respectively,
+// as per http://golang.org/pkg/crypto/aes/#NewCipher)
+//
+// The key will be padded to the given size if needed.
+// An IV is created as a series of NULL bytes of necessary length
 func New(size int, key string) (*Aes, error) {
     padded := make([]byte, size)
     copy(padded, []byte(key))
@@ -33,6 +41,9 @@ func (me *Aes) padSlice(src []byte) []byte {
     return src_padded
 }
 
+// Encrypt a slice of bytes, producing a new, freshly allocated slice
+//
+// Source will be padded with null bytes if necessary
 func (me *Aes) Encrypt(src []byte) []byte {
     if (len(src) % me.enc.BlockSize() != 0) {
         src = me.padSlice(src)
@@ -42,6 +53,9 @@ func (me *Aes) Encrypt(src []byte) []byte {
     return dst
 }
 
+// Decrypt a slice of bytes, producing a new, freshly allocated slice
+//
+// Source will be padded with null bytes if necessary
 func (me *Aes) Decrypt(src []byte) []byte {
     if (len(src) % me.dec.BlockSize() != 0) {
         src = me.padSlice(src)
